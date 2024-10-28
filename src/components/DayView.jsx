290478ef44d00d7905a27bb9f162ada34a8c941e
stevelw <steve@stevelw.co.uk>
2024-10-28 15:04:56 +0000

    implement DayView

--- src/components/DayView.jsx
+++ src/components/DayView.jsx
@@ -1,8 +1,26 @@
-function DayView({ dayData}) {
+function DayView({ dayData }) {
 
     return (
         <div id="day-view">
-            <h2>DayView</h2>
+            <h2>The next 4 hours</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Grams of CO<sub>2</sub> released per kilowatt-hour generated</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Average</td>
+                        <td>{dayData.average}</td>
+                    </tr>
+                    <tr>
+                        <td>Min – Max</td>
+                        <td>{dayData.min} - {dayData.max}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
