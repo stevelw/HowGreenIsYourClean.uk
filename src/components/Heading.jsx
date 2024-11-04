a45e99a07ff87031bd81bf123fa6e701d75344ad
stevelw <steve@stevelw.co.uk>
2024-11-04 08:53:49 +0000

    updates title to match domain

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,7 +5,7 @@ function Heading() {
     return (
         <header>
             <img src={logo} alt="icon of a grren-energy styled washing machine" width='100' height='100' />
-            <h1>How <span className="green">clean</span> is your wash?</h1>
+            <h1>How <span className="green">Green</span> Is Your Clean?</h1>
         </header>
     )
 }
