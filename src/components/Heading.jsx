e9aa040bfa500d5d6bfdbef7ee391b3d33468182
stevelw <steve@stevelw.co.uk>
2024-11-05 22:24:46 +0000

    make responsive

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,13 +1,19 @@
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
 
 function Heading() {
-
-    return (
-        <header>
-            <img src={logo} alt="icon of a grren-energy styled washing machine" width='100' height='100' />
-            <h1>How <span className="green">Green</span> Is Your Clean?</h1>
-        </header>
-    )
+  return (
+    <header style={{ gridArea: "header" }}>
+      <img
+        src={logo}
+        alt="icon of a grren-energy styled washing machine"
+        width="100"
+        height="100"
+      />
+      <h1>
+        How <span className="green">Green</span> Is Your Clean?
+      </h1>
+    </header>
+  );
 }
 
-export default Heading
+export default Heading;
